a3054a53b0d7812f9a11e078e68522a7ba1cd855
wwt1983 <wwt1983@yandex.ru>
2024-04-30 01:56:49 +0500

    del index css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { Provider, ErrorBoundary } from "@rollbar/react";
 
 const rollbarConfig = {
