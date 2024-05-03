cba100704d4edd43902f8bfcfbd27d173f5e756e
wwt1983 <wwt1983@yandex.ru>
2024-05-03 19:17:19 +0500

    update local

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         setIsLoading(true);
 
         const response = await fetch(
-          `https://cold-swans-attend.loca.lt/telegram/bot`,
+          `https://floppy-hoops-begin.loca.lt/telegram/bot`,
           {
             method: "POST",
             headers: {
