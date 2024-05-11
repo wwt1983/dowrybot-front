6e04303b41a280ae6d26f49f7c45e1ef6a10b003
wwt1983 <wwt1983@yandex.ru>
2024-05-11 17:44:51 +0500

    update url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const BACKAND_URL = 'https://www.dowray.ru/'
-//export const BACKAND_URL = 'https://fair-queens-buy.loca.lt/'
+//export const BACKAND_URL = 'https://www.dowray.ru/'
+export const BACKAND_URL = 'https://silver-kings-accept.loca.lt/'
