5c998250a89f6ddab3567f2cd6b409553f5ab26f
wwt1983 <wwt1983@yandex.ru>
2024-05-12 15:11:38 +0500

    develop: add url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 //export const BACKAND_URL = 'https://www.dowray.ru/'
-export const BACKAND_URL = 'https://silver-kings-accept.loca.lt/'
+export const BACKAND_URL = 'https://clean-papers-pump.loca.lt/'
