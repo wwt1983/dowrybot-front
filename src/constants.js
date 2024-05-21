2613d894025fc4b029f8247d4b7b5f1d0cef3f87
wwt1983 <wwt1983@yandex.ru>
2024-05-21 15:20:03 +0500

    dev update url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://red-snakes-look.loca.lt/';
+export const BACKAND_URL = 'https://small-facts-fetch.loca.lt/';
 
 export const Status = {
   process: "In progress",
