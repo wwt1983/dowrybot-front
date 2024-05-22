5019ffbb32c68e92ff18b59721c35a164ccdd552
wwt1983 <wwt1983@yandex.ru>
2024-05-22 16:19:02 +0500

    develop updatre url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://small-facts-fetch.loca.lt/';
+export const BACKAND_URL = 'https://grumpy-melons-ask.loca.lt/';
 
 export const Status = {
   process: "In progress",
