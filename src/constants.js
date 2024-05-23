c2311631dfa734737c6d8cf32835ff1eb6818e47
wwt1983 <wwt1983@yandex.ru>
2024-05-23 12:31:31 +0500

    develop url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://full-cities-fetch.loca.lt/';
+export const BACKAND_URL = 'https://ripe-crabs-wear.loca.lt/';
 
 export const Status = {
   process: "In progress",
