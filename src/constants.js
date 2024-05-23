29b1b294bec939ead24595801a6b18c9cf24983f
wwt1983 <wwt1983@yandex.ru>
2024-05-24 04:12:39 +0500

    develop update url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://crazy-paths-design.loca.lt/';
+export const BACKAND_URL = 'https://dry-ideas-stand.loca.lt/';
 
 export const Status = {
   process: "In progress",
