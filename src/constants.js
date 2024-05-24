3ba40f5ffe7937d4d0c4ac3b0d4cf3ebcf24cb2f
wwt1983 <wwt1983@yandex.ru>
2024-05-24 11:02:11 +0500

    dev u url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://solid-cars-visit.loca.lt/';
+export const BACKAND_URL = 'https://wide-nails-know.loca.lt/';
 
 export const Status = {
   process: "In progress",
