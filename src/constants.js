07d1748a1281a627d2987b2a34f8ffcfef730601
wwt1983 <wwt1983@yandex.ru>
2024-06-05 22:28:07 +0500

    develop update url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://pretty-islands-argue.loca.lt/';
+export const BACKAND_URL = 'https://tall-guests-kiss.loca.lt/';
 
 export const Status = {
   process: "In progress",
