00daee47918657dfa03ef6b88fe8e997a25b0a21
wwt1983 <wwt1983@yandex.ru>
2024-06-10 19:36:09 +0500

    develop u url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://moody-toys-tickle.loca.lt/';
+export const BACKAND_URL = 'https://eighty-dryers-hunt.loca.lt/';
 
 export const Status = {
   process: "In progress",
