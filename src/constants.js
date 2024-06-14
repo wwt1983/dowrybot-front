42f8b99f01d5a67f41c2d9af67fb273c44985546
wwt1983 <wwt1983@yandex.ru>
2024-06-14 16:38:31 +0500

    develop u url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = 'https://three-books-grab.loca.lt/';
+export const BACKAND_URL = 'https://purple-states-film.loca.lt/';
 
 export const Status = {
   process: "In progress",
