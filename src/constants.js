6bb1a9be6ee3dff58e6561e07b26428a4f374f9d
wwt1983 <wwt1983@yandex.ru>
2024-06-19 15:34:29 +0500

    dev update url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = "https://spicy-ends-heal.loca.lt/";
+export const BACKAND_URL = "https://hot-points-happen.loca.lt/";
 
 export const Status = {
   process: "In progress",
