e02ce1fe413eea14584a1279cd2cd70a962e7413
wwt1983 <wwt1983@yandex.ru>
2024-07-04 00:20:17 +0500

    develop u url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = "https://open-streets-slide.loca.lt/";
+export const BACKAND_URL = "https://fair-tires-sink.loca.lt/";
 
 export const Status = {
   process: "In progress",
