25d2b4fd44ed46a6a5c0fac3ca44bd73554ca0a4
wwt1983 <wwt1983@yandex.ru>
2024-07-08 20:23:28 +0500

    develop u url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const BACKAND_URL = "https://www.dowray.ru/";
-export const BACKAND_URL = "https://pink-coats-divide.loca.lt/";
+export const BACKAND_URL = "https://deep-meals-joke.loca.lt/";
 
 export const Status = {
   process: "In progress",
