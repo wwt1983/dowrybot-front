9c13166158b62871c2af5c6b84746fac0bc1a43e
wwt1983 <wwt1983@yandex.ru>
2024-07-11 12:24:07 +0500

    del puppeter

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Button from "./components/button/Button";
 
 import { useTelegram } from "./hooks/useTelegram";
 import { sendData, getOffers } from "./db/fetch";
-import { getParseWbInfo } from "./parse";
 
 const override = {
   display: "block",
@@ -19,8 +18,6 @@ function App() {
   const [orders, setOrders] = useState([]);
   const { tg, queryId, id } = useTelegram();
 
-  const t = getParseWbInfo(234075582);
-  console.log(t);
 
   const onSendData = useCallback(async () => {
     try {
