0b88b127633f17491256f70226cebe87fc5af0cc
wwt1983 <wwt1983@yandex.ru>
2024-08-19 13:55:13 +0500

    test

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const BACKAND_URL = "https://www.dowray.ru:3000/";
 //export const BACKAND_URL = "https://deep-meals-joke.loca.lt/";
 
+
 export const Status = {
   process: "In progress",
   scheduled: "Scheduled",
