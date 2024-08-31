690bc5abb9ffb638e7eefbc4a0886717c748619b
wwt1983 <wwt1983@yandex.ru>
2024-09-01 03:48:09 +0500

    return

--- src/db/fetch.js
+++ src/db/fetch.js
@@ -1,40 +1,43 @@
 import { BACKAND_URL } from "../constants";
 import { dtoToOffers } from "./convertDto";
-import axios from "axios";
 
 export async function sendData(items, queryId, id) {
-  return await axios.post(`${BACKAND_URL}telegram/bot`, {
-    title: items.title,
-    //cash: items.cash,
-    //keys: items.keys,
-    query_id: queryId,
-    id: id,
-    articul: items.articul,
-    offerId: items.id,
-    //image: items.image,
-    //priceForYou: items.priceForYou,
-    //priceWb: items.priceWb,
-    //description: items.description,
-    //location: items.location,
-    //positionOnWB: items.positionOnWB,
+  return await fetch(`${BACKAND_URL}telegram/bot`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      title: items.title,
+      //cash: items.cash,
+      //keys: items.keys,
+      query_id: queryId,
+      id: id,
+      articul: items.articul,
+      offerId: items.id,
+      //image: items.image,
+      //priceForYou: items.priceForYou,
+      //priceWb: items.priceWb,
+      //description: items.description,
+      //location: items.location,
+      //positionOnWB: items.positionOnWB,
+    }),
   });
 }
 
 export async function getOffers() {
   try {
-    const response = await axios.get(
-      `https://api.airtable.com/v0/appVMEtut0NWayq26/tblGUNSCgAEwhxchx?cellFormat=json&filterByFormula==OR({Status}="In progress", {Status}="Scheduled")`,
-      {
-        headers: {
-          "Content-Type": "application/json;charset=utf-8",
-          Authorization: `Bearer patInEeuukHn2f8F0.8e6d4fb99e673b8dffb1debb1bb823f6ad678f79638b502a30b35eb91677f291`,
-        },
-      }
-    );
+    const response = await fetch(`${BACKAND_URL}airtable/offers`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
 
-    if (!response || !response.data.records) return [];
-
-    return dtoToOffers(response.data.records);
+    const result = await response.json();
+    if(!result || !result.records) return []
+    
+    return dtoToOffers(result.records);
   } catch (e) {
     console.log("getOffers", e);
     return [];
