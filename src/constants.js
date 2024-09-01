9735aab5f8d84d0040fc8a2ce1cb88b807b95a48
wwt1983 <wwt1983@yandex.ru>
2024-09-01 17:44:33 +0500

    del port

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BACKAND_URL = "https://dowray.ru:8080/";
+export const BACKAND_URL = "https://dowray.ru/";
 //export const BACKAND_URL = 'https://tall-taxis-reply.loca.lt/';
 
 export const Status = {
