914ef5e5cee3c85fa78d55e0d65d5ae40a7de74a
wwt1983 <wwt1983@yandex.ru>
2024-09-16 22:15:00 +0500

    add test url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
 export const BACKAND_URL = "https://dowray.ru/";
+export const TEST_BACKAND_URL = "https://kind-chicken-repair.loca.lt/";
+
 export const BACKGROUND_URL_AIRTABLE =
   'https://api.airtable.com/v0/appVMEtut0NWayq26/tblGUNSCgAEwhxchx?cellFormat=json&filterByFormula==OR({Status}="In progress",{Status}="Scheduled")';
 //export const BACKAND_URL = 'https://tall-taxis-reply.loca.lt/';
--- src/db/fetch.js
+++ src/db/fetch.js
@@ -1,29 +1,38 @@
-import { BACKAND_URL, BACKGROUND_URL_AIRTABLE } from "../constants";
+import {
+  BACKAND_URL,
+  BACKGROUND_URL_AIRTABLE,
+  TEST_BACKAND_URL,
+} from "../constants";
 import { dtoToOffers } from "./convertDto";
 import axios from "axios";
 
 export async function sendData(items, queryId, id) {
-  return await fetch(`${BACKAND_URL}telegram/bot`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({
-      title: items.title,
-      //cash: items.cash,
-      //keys: items.keys,
-      query_id: queryId,
-      id: id,
-      //articul: items.articul,
-      offerId: items.id,
-      //image: items.image,
-      //priceForYou: items.priceForYou,
-      //priceWb: items.priceWb,
-      //description: items.description,
-      //location: items.location,
-      //positionOnWB: items.positionOnWB,
-    }),
-  });
+  return await fetch(
+    `${
+      items.title.includes("тест") ? TEST_BACKAND_URL : BACKAND_URL
+    }telegram/bot`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({
+        title: items.title,
+        //cash: items.cash,
+        //keys: items.keys,
+        query_id: queryId,
+        id: id,
+        //articul: items.articul,
+        offerId: items.id,
+        //image: items.image,
+        //priceForYou: items.priceForYou,
+        //priceWb: items.priceWb,
+        //description: items.description,
+        //location: items.location,
+        //positionOnWB: items.positionOnWB,
+      }),
+    }
+  );
 }
 
 export async function getOffers() {
