2285ca17f1bc7316c826e0336a99652cdccdbe4f
wwt1983 <wwt1983@yandex.ru>
2024-09-25 15:47:51 +0500

    upd url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const BACKAND_URL = "https://dowray.ru/";
-export const TEST_BACKAND_URL = "https://clever-eyes-brush.loca.lt/";
+export const TEST_BACKAND_URL = "https://fifty-hairs-think.loca.lt/";
 
 export const BACKGROUND_URL_AIRTABLE =
   'https://api.airtable.com/v0/appVMEtut0NWayq26/tblGUNSCgAEwhxchx?cellFormat=json&filterByFormula==OR({Status}="In progress",{Status}="Scheduled")';
