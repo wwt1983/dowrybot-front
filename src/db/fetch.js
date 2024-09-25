5020dba4adcb417b13ea7b94c0292ada75ecd970
wwt1983 <wwt1983@yandex.ru>
2024-09-25 16:01:22 +0500

    test

--- src/db/fetch.js
+++ src/db/fetch.js
@@ -33,13 +33,10 @@ export async function sendData(items, queryId, id) {
 
 export async function getOffers() {
   try {
-    
-    const response = await fetch(`${TEST_BACKAND_URL}airtable/offers`, {
+    const response = await axios.get(`${TEST_BACKAND_URL}airtable/offers`, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
     });
+    console.log(response)
     if (!response || !response.data.records) return [];
 
     return dtoToOffers(response.data.records);
